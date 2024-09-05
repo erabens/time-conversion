52c468cb8f6a3546b722a2a00d0198f30d108313
erabens <51397121+erabens@users.noreply.github.com>
2024-09-05 13:56:16 +0000

    edit time.java

--- time.java
+++ time.java
@@ -1,1 +1,17 @@
+public class time{
+    public static void main(String[] args){
 
+        int x = 112572; //initial seconds
+
+        int sec = 112572 % 60; //remaining seconds that are not FULL minutes
+        x /= 60; //convert to minutes
+
+        int min = x % 60; //remaining minutes that are not FULL hours
+        x /= 60; //convert to hours
+
+        int hr = x % 24; //remaining hours that are not FULL days
+        x /= 24; //convert to days
+
+        System.out.println(x + " days, " + hr + " hours, " + min + " minutes, and " + sec + " seconds");
+    }
+}
